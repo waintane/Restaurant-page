ab45ac43e466cb0b48d0dcba75de30488b6ce73f
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-07-08 22:03:33 -0400

    ajout du css du menu burger

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ let burgerButton ={
     object : document.querySelector(".navigation-burger-button svg"),
     active : false,
 }
+let menuBurger = {
+    object : document.querySelector(".menu"),
+}
 
 
 burgerButton.object.addEventListener("click", () => {
     if(burgerButton.active === false){
         burgerButton.object.classList.add("rotate");
+        menuBurger.object.setAttribute('style', 'left: 0%');
         burgerButton.active = true;
     }
     else{
         burgerButton.object.classList.remove("rotate");
+        menuBurger.object.setAttribute('style', 'left: -100%');
         burgerButton.active = false;
     }
 })
