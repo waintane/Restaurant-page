d2c5d2d0f54a9d31013eb52823bad057dff8ba25
Olivier Paradis-Tardif <olitarpar@hotmail.com>
2022-07-09 20:16:42 -0400

    implementation d'un header.php dans toutes les pages

---
+++ public/_header.php
@@ -0,0 +1,20 @@
+
+<!-- header du site -->
+<header class="img">
+        <div class="navigation">
+            <div class="navigation-burger-button">
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <?php echo "<p>$pageTitle</p>"; ?>
+            </div>
+        </div>
+        <div class="menu">
+            <a href="index.php">Accueil</a>
+            <a href="menu.php">Menu</a>
+            <a href="contact.php">Contact</a>
+        </div>
+        <div class="text-image-header">
+            <h3>Burgers délicieux</h3>
+            <p>Dit bonjour <br> aux meilleurs burgers de <br> l'île</p>
+            <p>Accessible à Montréal à l'année longue</p>
+        </div>
+</header>
---
+++ public/contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<?php
+$pageTitle = "Contact"
+?>
+
+<html lang="fr-ca">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/normalize.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>burger bottom</title>
+
+    
+</head>
+<body>
+
+    <!-- Appelle le header.php -->
+    <?php
+    include "_header.php"
+    ?>
+
+    <main>
+        <section>
+            
+        </section>
+    </main>
+
+    <!-- Appelle le footer.php -->
+    <?php
+    include "_footer.php"
+    ?>
+
+    <script src="../dist/main.js"></script>
+</body>
+</html>
